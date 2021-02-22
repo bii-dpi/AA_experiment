dce5e2668fe10fd96d6760565ec57b629587bc20
bii-dpi <76100319+bii-dpi@users.noreply.github.com>
2021-02-22 13:21:25 +0800

    Add files via upload

--- grad_hk2.py
+++ grad_hk2.py
@@ -3,31 +3,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
+lda = 10000000
 # =======================================
 class encoder_net(nn.Module):
-
     def __init__(self):
-        super(encoder_net,self).__init__()
-        self.fc1 = nn.Linear(1,2,bias=True)
+        super(encoder_net, self).__init__()
+        self.fc1 = nn.Linear(1, 2, bias=True)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.fc1(x)
         x = torch.sigmoid(x)
         return x
+
+
 # =======================================
 class think_net(nn.Module):
-
-    def __init__(self,nhidden=32):
-        super(think_net,self).__init__()
-        self.fc1 = nn.Linear(2      ,nhidden,bias=True)
-        self.fc2 = nn.Linear(nhidden,nhidden,bias=True)
-        self.fc3 = nn.Linear(nhidden,1 ,bias=True)
-        #with torch.no_grad():
+    def __init__(self, nhidden=32):
+        super(think_net, self).__init__()
+        self.fc1 = nn.Linear(2, nhidden, bias=True)
+        self.fc2 = nn.Linear(nhidden, nhidden, bias=True)
+        self.fc3 = nn.Linear(nhidden, 2, bias=True)
+        # with torch.no_grad():
         #    self.fc1.weight.fill_(0.1)
         #    self.fc2.weight.fill_(0.1)
         #    self.fc3.weight.fill_(0.1)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.fc1(x)
         x = torch.sigmoid(x)
         x = self.fc2(x)
@@ -35,26 +37,29 @@ class think_net(nn.Module):
         x = self.fc3(x)
         x = torch.sigmoid(x)
         return x
+
+
 # =======================================
 class classifier_net(nn.Module):
     def __init__(self):
-        super(classifier_net,self).__init__()
-        self.fc1 = nn.Linear(2,2,bias=True)
-        self.fc2 = nn.Linear(2,2,bias=True)
-        #with torch.no_grad():
+        super(classifier_net, self).__init__()
+        self.fc1 = nn.Linear(2, 2, bias=True)
+        self.fc2 = nn.Linear(2, 2, bias=True)
+        # with torch.no_grad():
         #    self.fc1.weight.fill_(0.5)
         #    self.fc2.weight.fill_(1.0)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.fc1(x)
         x = torch.sigmoid(x)
         x = self.fc2(x)
         x = torch.sigmoid(x)
-        x = torch.softmax(x,dim=1)
+        x = torch.softmax(x, dim=1)
         return x
 
+
 # =======================================
-def grad_descend(tnet,z,niter=8,eps=0.1):
+def grad_descend(tnet, z, niter=8, eps=0.1):
 
     z_list = []
     z.retain_grad()
@@ -62,30 +67,36 @@ def grad_descend(tnet,z,niter=8,eps=0.1):
 
     for t in range(niter):
         y = tnet(z_list[t])
-        y.backward(torch.ones([ndata,1]))
+        y.backward(torch.ones([ndata, 2]))
         #print('t ',t,' : z ',z_list[t],' zgrad ',z_list[t].grad,' y ',y)
         with torch.no_grad():
-            a = z_list[t] - eps*z_list[t].grad
+            a = z_list[t] - eps * z_list[t].grad
+            #print(z_list[t].grad)
             a.requires_grad_(True)
         z_list[t].grad.zero_()
         z_list.append(a)
 
     return z_list
+
+
 # =======================================
 def print_param(net):
 
     for param in net.parameters():
         print(param.data)
+
+
 # =======================================
 def prepare_data(ndata):
-    
-    x = torch.rand([ndata,1])
-    y = torch.squeeze((x[:,]>0.5).long())
 
-    return x,y
+    x = torch.rand([ndata, 1])
+    y = torch.squeeze((x[:,] > 0.5).long())
+
+    return x, y
+
+
 # =======================================
-
-if __name__=='__main__':
+if __name__ == "__main__":
 
     torch.manual_seed(263839)
     enet = encoder_net()
@@ -95,40 +106,27 @@ if __name__=='__main__':
     tnet.train()
     cnet.train()
 
-    end2end_param = list(enet.parameters()) + \
-                    list(tnet.parameters()) + \
-                    list(cnet.parameters())
-    net_opt = optim.Adam(end2end_param,weight_decay=0.01)
-    #net_opt = optim.SGD(end2end_param,lr=0.001)
+    end2end_param = (
+        list(enet.parameters()) + list(tnet.parameters()) + list(cnet.parameters())
+    )
+    net_opt = optim.Adam(end2end_param, weight_decay=0.01)
 
     ndata = 64  # total number of data point, feed in a batch
-    nepoch = 100000
+    nepoch = 10000
 
     for epoch in range(nepoch):
 
-        x,label = prepare_data(ndata) # prepare new data every epoch
-        
-        z = enet(x)                   # encode
-        z_list = grad_descend(tnet,z) # perform gradient descend
-        #print('z list first ',z_list)
-        zbatch = z_list[-1]           # feed the final time step z into cnet
-        #print('zbatch ',zbatch)
-        pred = cnet(zbatch)           # classify optimized z
-        #print('pred ',pred,'\n label ',label)
+        x, label = prepare_data(ndata)  # prepare new data every epoch
+        position = torch.FloatTensor([[i.item(), i.item()] for i in label])
 
-        loss = nn.functional.cross_entropy(pred,label)
-
-        if epoch%(nepoch//1000)==0:
-            print('loss ',loss)
-            # plot tnet surface here
+        z = enet(x)  # encode
+        z_list = grad_descend(tnet, z)  # perform gradient descend
+        zbatch = z_list[-1]  # feed the final time step z into cnet
+        pred = cnet(zbatch)  # classify optimized z
+        loss = nn.functional.cross_entropy(pred, label)
+        loss = loss + torch.mul(lda, torch.mean((position - zbatch) ** 2))
+        if epoch % (nepoch // 1000) == 0:
+            print("loss ", loss)
             # plot_tnet()
         loss.backward()
-        net_opt.step()                # update weights end2end
-        
-        #print('enet parameters =============== ')
-        #print_param(enet)
-        #print('tnet parameters =============== ')
-        #print_param(tnet)
-        #print('cnet parameters =============== ')
-        #print_param(cnet)
-
+        net_opt.step()  # update weights end2end
